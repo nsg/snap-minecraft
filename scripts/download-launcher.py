4bf0f36e0e9c3ec4b217a373395faf7bbeb2d5dc
kz6fittycent <jimmy.tigert@gmail.com>
2019-04-20 18:50:41 -0500

    Update download-launcher.py

--- scripts/download-launcher.py
+++ scripts/download-launcher.py
@@ -2,8 +2,24 @@
 
 import os.path
 from urllib.request import urlretrieve 
+import subprocess
+import os
+import re
+
+
+#def main():
+#    urlretrieve("https://s3.amazonaws.com/Minecraft.Download/launcher/Minecraft.jar", "Minecraft.jar")
+#main()
+
+# Dowload new launcher...
+#!/usr/bin/env python3
+
 
 
 def main():
-    urlretrieve("https://s3.amazonaws.com/Minecraft.Download/launcher/Minecraft.jar", "Minecraft.jar")
+    urlretrieve("https://launcher.mojang.com/download/linux/x86_64/minecraft-launcher_2.1.2482.tar.gz", "minecraft-launcher.tar.gz")
+    
+    subprocess.call(['tar','-xzf','minecraft-launcher.tar.gz'])
+    
 main()
+
